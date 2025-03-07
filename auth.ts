dac1a154089b7896f66913bbb1a2a7734f31ddf2
cmelonijs <carlomeloni63@gmail.com>
2025-03-07 11:36:51 +0100

    feat: persist session cart

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from "next/headers";
 
 export const config = {
   pages: {
@@ -70,9 +71,10 @@ export const config = {
       return session;
     },
 
-    async jwt({ token, user }: any) {
+    async jwt({ token, user, trigger }: any) {
       // assign user fields to the token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
         // if user has no name, then use the email
         if (user.name === "NO_NAME") {
@@ -83,6 +85,34 @@ export const config = {
             where: { id: user.id },
             data: { name: token.name },
           });
+        }
+
+        if (trigger === "signIn" || trigger === "signUp") {
+          const cookiesObejct = await cookies();
+          const sessionCartId = cookiesObejct.get("sessionCartId")?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              //delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // assign new cart
+              await prisma.cart.update({
+                where: {
+                  id: sessionCart.id,
+                },
+                data: {
+                  userId: user.id,
+                },
+              });
+            }
+          }
         }
       }
 
