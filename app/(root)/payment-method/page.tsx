526335ce4d32b8779a1adeed6453dfeeedcc3783
cmelonijs <carlomeloni63@gmail.com>
2025-03-10 10:00:47 +0100

    feat: payment method page start implementation

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,28 @@
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import { Metadata } from "next";
+import PaymentMethodForm from "./payment-method-form";
+
+export const metadata: Metadata = {
+  title: "Select a pyament method",
+};
+
+const PaymentMethodPage = async () => {
+  const session = await auth();
+
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+    </>
+  );
+};
+
+export default PaymentMethodPage;
