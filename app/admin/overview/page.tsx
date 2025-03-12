e331733ccf05d1071e384dce3e052eaa282a143e
cmelonijs <carlomeloni63@gmail.com>
2025-03-12 17:53:38 +0100

    feat: protecting admin pages with admin auth guard

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -14,12 +14,14 @@ import { BadgeDollarSign, Barcode, CreditCardIcon, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import Charts from "./charts";
+import { requireAdmin } from "@/lib/guard";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
 };
 
 const AdminOverviewPage = async () => {
+  await requireAdmin();
   const session = await auth();
 
   if (session?.user?.role !== "admin") {
