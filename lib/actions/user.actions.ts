4fd88e174422553d09147f49cc3fb5194be95a42
cmelonijs <carlomeloni63@gmail.com>
2025-03-12 12:00:01 +0100

    feat: update profile actiona and schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -162,3 +162,39 @@ export async function updateUserPaymentMethod(
     formatError(err);
   }
 }
+
+// update the user profile
+export async function updateProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+
+    if (!currentUser) {
+      throw new Error("User not found");
+    }
+
+    await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return {
+      success: true,
+      message: "User has been updated successfully",
+    };
+  } catch (err) {
+    return {
+      success: false,
+      message: formatError(err),
+    };
+  }
+}
