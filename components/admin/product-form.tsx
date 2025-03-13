60059d891c16df6e6ea8f5b066a779f2099a0b05
cmelonijs <carlomeloni63@gmail.com>
2025-03-13 17:44:24 +0100

    feat: add an upload images button inside product form

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -21,6 +21,9 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { createProduct, updateProduct } from "@/lib/actions/product.actions";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
+import { UploadButton } from "@/lib/uploadthing";
 
 const ProductForm = ({
   type,
@@ -78,6 +81,8 @@ const ProductForm = ({
       }
     }
   };
+
+  const images = form.watch("images");
 
   return (
     <Form {...form}>
@@ -236,6 +241,43 @@ const ProductForm = ({
         </div>
         <div className="upload-field flex flex-col items-start md:flex-row gap-5">
           {/* Images */}
+          <FormField
+            control={form.control}
+            name="images"
+            render={() => (
+              <FormItem className="w-full">
+                <FormLabel>Images</FormLabel>
+                <Card>
+                  <CardContent className="space-y-2 mt-2 min-h-48">
+                    <div className="flex-start space-x-2">
+                      {images.map((image: string) => (
+                        <Image
+                          key={image}
+                          src={image}
+                          alt="product image"
+                          className="w-20 h-20 object-cover object-center rounded-sm"
+                          width={100}
+                          height={100}
+                        />
+                      ))}
+                      <FormControl>
+                        <UploadButton
+                          endpoint="imageUploader"
+                          onClientUploadComplete={(res: { url: string }[]) => {
+                            form.setValue("images", [...images, res[0].url]);
+                          }}
+                          onUploadError={(error: Error) => {
+                            toast.error(error.message);
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </CardContent>
+                </Card>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <div className="upload-field">Upload field</div>
         <div>{/* Description */}</div>
