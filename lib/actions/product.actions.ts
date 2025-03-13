43493826de6d29727eae1cc457a2fc6539a20551
cmelonijs <carlomeloni63@gmail.com>
2025-03-13 11:55:16 +0100

    feat: create an action to get all productas

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,7 +2,7 @@
 
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
-import { LATEST_PRODUCT_LIMIT } from "../constants";
+import { PAGE_SIZE } from "../constants";
 
 // get latest products
 export async function getLatestProducts() {
@@ -18,9 +18,33 @@ export async function getLatestProducts() {
 }
 
 // get signle product by slug
-
 export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
+}
+
+// get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
