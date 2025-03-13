a1e75436a31f1812cdde5825d61a0565726d2893
cmelonijs <carlomeloni63@gmail.com>
2025-03-13 14:15:03 +0100

    feat: action to delete product and button to delete it

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,4 @@
+import DeleteDialog from "@/components/shared/delete-dialog";
 import Pagination from "@/components/shared/pagination";
 import { Button } from "@/components/ui/button";
 import {
@@ -8,7 +9,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { getAllProducts } from "@/lib/actions/product.actions";
+import { deleteProduct, getAllProducts } from "@/lib/actions/product.actions";
 import { requireAdmin } from "@/lib/guard";
 import { formatCurrency, formatId } from "@/lib/utils";
 import Link from "next/link";
@@ -64,6 +65,7 @@ const AdminProductsPage = async (props: {
                 <Button variant="outline">
                   <Link href={`admin/products/${product.id}`}>Edit</Link>
                 </Button>
+                <DeleteDialog id={product.id} action={deleteProduct} />
               </TableCell>
             </TableRow>
           ))}
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import { prisma } from "@/db/prisma";
-import { convertToPlainObject } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
 import { PAGE_SIZE } from "../constants";
 import { LATEST_PRODUCT_LIMIT } from "../constants";
+import { revalidatePath } from "next/cache";
 
 // get latest products
 export async function getLatestProducts() {
@@ -48,4 +49,35 @@ export async function getAllProducts({
     data,
     totalPages: Math.ceil(dataCount / limit),
   };
+}
+
+// delete product
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!productExists) throw new Error("Pruuc is not found");
+
+    await prisma.product.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    revalidatePath("/admin/products");
+
+    return {
+      success: true,
+      message: "Product deleted successfully",
+    };
+  } catch (err) {
+    return {
+      success: false,
+      message: formatError(err),
+    };
+  }
 }
