1a6c79f5a34bbb40da8350157e1ad23843270d5b
cmelonijs <carlomeloni63@gmail.com>
2025-03-14 15:42:30 +0100

    fix: minor fix

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import NotFoungPage from "@/app/not-found";
 import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
@@ -17,7 +18,7 @@ const UpdateUserPage = async (props: { params: Promise<{ id: string }> }) => {
   return (
     <div className="space-y-8 max-w-lg mx-auto">
       <h1 className="h2-bold">Update User</h1>
-      <UpdateUserForm user={user} />
+      <UpdateUserForm user={user as any} />
     </div>
   );
 };
